e5fdcd49f22dcc8988b09b854781ccf1dc14c73d
Leo <zjzjzjlolicon@gmail.com>
2021-01-24 02:04:10 -0500

    confirmo
    confirmadisimo sela recontra come

---
+++ public/Emersonselacome.ts
@@ -0,0 +1,13 @@
+export class emersonselacome {
+    constructor() {
+        this.selacomeid = true;
+        this.selacomeENTERA = true;
+        this.fecha = new Date();
+
+    }
+
+    selacomeid: boolean;
+    selacomenombre: string;
+    selacomeENTERA: boolean;
+    fecha: Date;
+}
